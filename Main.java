38b3107014a45cab877214597ac30b095f8ea11a
graysari <165754890+graysari@users.noreply.github.com>
2024-05-08 14:21:01 +0000

    1

---
+++ Main.java
@@ -0,0 +1,57 @@
+import java.util.*;
+public class Main{
+    private int sel;
+    
+    public int getSel() {
+        return this.sel;
+    }
+
+    public void setSel(int sel) {
+       this.sel = sel;
+    }
+
+    public void newScr(){
+        System.out.println("\n*******************************************************************\n");
+    }
+    public void select(){
+        Scanner inp = new Scanner(System.in);
+        
+        System.out.print("\nPlease Select: ");
+
+        setSel(inp.nextInt());
+    }
+
+    public void mainScr(){
+        System.out.println("1. Login");        
+        System.out.println("2. Create account");        
+        System.out.println("3. Exit");
+
+        select();
+        
+        System.out.println(getSel());
+    
+    switch (getSel()) {
+        case 1: 
+                newScr();
+                logIn login = new logIn();
+                break;
+        case 2: System.out.println("This is Create account");
+            
+                break;
+        case 3: System.out.println("This is Exit");
+            
+                break;                
+    
+        default:
+                newScr();
+                System.out.println("Invalid Number. Please select a number again.");
+                mainScr();
+                break;
+    }
+
+    }
+    public static void main(String[] args) {
+    Main main = new Main();
+    main.mainScr();
+}
+}
---
+++ logIn.java
@@ -0,0 +1,29 @@
+import java.text.BreakIterator;
+
+public class logIn {
+    public logIn(){
+        Main main = new Main();
+        System.out.println("1. Enter your ID number and Pin code");
+        System.out.println("2. Back");
+
+        main.select();
+
+        switch (main.getSel()) {
+            case 1:
+                System.out.println("Please enter you ID number: ");
+                
+                break;
+            case 2:
+                main.newScr();
+                main.mainScr();
+                break;
+        
+            default:
+                main.newScr();
+                System.out.println("Invalid Number please select again.");
+                new logIn();
+                main.select();
+                break;
+        }
+    }
+}
